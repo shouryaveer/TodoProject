a2e2fefd68c7f81a22ca861f7a53985061803dd2
shouryaveer <bajrang.swansoft@gmail.com>
2021-06-01 17:03:56 +0530

    Refactored todolist app to use Class-based Views & ModelForms for CRUD operations

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = "todo"
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('add', views.add, name="add"),
-    path("remove", views.remove, name="remove")
+    path('', views.TasksListView.as_view(), name="index"),
+    path('add', views.TasksCreateView.as_view(), name="add"),
+    path('update/<int:pk>', views.TasksUpdateView.as_view(), name="update"),
+    path("delete", views.TasksDeleteView.as_view(), name="delete_form"),
 ]
